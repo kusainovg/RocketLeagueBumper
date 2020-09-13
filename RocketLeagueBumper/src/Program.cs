00c351fb8f55f1ea32c23c6591a5085648957ca8
Gamzat <gamzat54321@yandex.ru>
2020-09-13 18:55:11 +0300

    Initial commit

---
+++ RocketLeagueBumper/src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RocketLeagueBumper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            System.Threading.Thread.Sleep(20000);
+        }
+    }
+}
